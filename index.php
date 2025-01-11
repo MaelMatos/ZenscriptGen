e7465de681e4fedb27fbd68902d8231236119568
MaelMatos <145067851+MaelMatos@users.noreply.github.com>
2025-01-11 08:25:24 -0300

    craftgrid v1

--- index.php
+++ index.php
@@ -9,17 +9,90 @@
 <body>
     <?php
 $materials = array(
+    array('none', 'null'),
     array('stone', 'minecraft:stone'),
     array('wood', 'minecraft:log'),
     array('dirt', 'minecraft:dirt'),
     array('grass', 'minecraft:grass')
 );
-foreach($materials as $material){
+function GenerateMaterial($materials){
+    $mod = "minecraft";
+    foreach($materials as $material){
+        if (!(explode(':',$material[0])[0] == $mod)){
+            $mod = explode(':',$material[0])[0];
+            echo '<optgroup label="'.$mod.'" class="tooltip"></optgroup>';
+        }
+        echo '<option value="'.$material[1].'" class="tooltip">'.$material[0].'</option>';
+    }
+}
+/* foreach($materials as $material){
     echo "name:".$material[0]."<br>";
     echo "id:".$material[1]."<br>";
-}
+} */
 ?>
-
+<div class="crafting">
+    <select name="1" id="" class="invslot">
+        <?php GenerateMaterial($materials);?>
+    </select>
+    <select name="2" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+        ?>
+    </select>
+    <select name="3" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+        ?>
+    </select>
+    <br>
+    <select name="4" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+       ?>
+    </select>
+    <select name="5" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+       ?>
+    </select>
+    <select name="6" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+       ?>
+    </select>
+    <br>
+    <select name="7" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+       ?>
+    </select>
+    <select name="8" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+       ?>
+    </select>
+    <select name="9" id="" class="invslot">
+        <?php
+        foreach($materials as $material){
+            echo '<option value="'.$material[1].'">'.$material[0].'</option>';
+        }
+       ?>
+    </select>
+</div>
 <script src="Tooltips.js"></script>
 </body>
 </html>
